4501785cede7b22f0e6662ca7895dd8e38da1d8e
Anceps <ancspeepyalt2tg4gw@gmail.com>
2026-05-29 09:53:01 +0000

    Update Checkpoint-SFX.hpp

--- src/Checkpoint-SFX.hpp
+++ src/Checkpoint-SFX.hpp
@@ -4,5 +4,5 @@
 #include <filesystem>
 
 namespace checkpoint_mod {
-    void playCheckpointSound(bool enabled, std::filesystem::path const &file, char const *fallback, double pitchVariance = 0.0);
+    void playCheckpointSound(bool enabled, std::filesystem::path const &file, char const *fallback, double pitchVariance = 0.15);
 }
