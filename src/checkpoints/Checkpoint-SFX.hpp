07b103d91a9147d163b84d13591df9fb1cff6543
Anceps <ancspeepyalt2tg4gw@gmail.com>
2026-07-06 07:55:41 +0000

    Add files via upload

---
+++ src/checkpoints/Checkpoint-SFX.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <Geode/Geode.hpp>
+#include <filesystem>
+
+namespace checkpoint_mod
+{
+    void playCheckpointSound(bool enabled, std::filesystem::path const &file, char const *fallback);
+}
