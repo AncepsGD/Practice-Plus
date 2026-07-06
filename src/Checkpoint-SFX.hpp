0ad705d7070676634f6ec7fc35402fe6f62e3471
Anceps <ancspeepyalt2tg4gw@gmail.com>
2026-07-06 07:57:29 +0000

    Delete src/Checkpoint-SFX.hpp

--- src/Checkpoint-SFX.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <Geode/Geode.hpp>
-#include <filesystem>
-
-namespace checkpoint_mod
-{
-    void playCheckpointSound(bool enabled, std::filesystem::path const &file, char const *fallback);
-}
